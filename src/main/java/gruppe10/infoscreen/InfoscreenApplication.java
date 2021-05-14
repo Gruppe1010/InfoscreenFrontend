41263222e4eea15bde0e5fef99251f38d8a8f9ea
Vibs <vibej@hotmail.com>
2021-05-14 11:14:39 +0200

    Initial commit

---
+++ src/main/java/gruppe10/infoscreen/InfoscreenApplication.java
@@ -0,0 +1,13 @@
+package gruppe10.infoscreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoscreenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfoscreenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gruppe10/infoscreen/InfoscreenApplicationTests.java
@@ -0,0 +1,13 @@
+package gruppe10.infoscreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoscreenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
