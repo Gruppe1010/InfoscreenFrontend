39a0ea54201f063f3d3f07f2938623c5185fbe3b
Vibs <vibej@hotmail.com>
2021-05-17 08:22:43 +0200

    linker rigtigt til image-filer

--- src/main/resources/static/js/slideshow.js
+++ src/main/resources/static/js/slideshow.js
@@ -1,1 +1,13 @@
 alert("HEj herjeheejej");
+
+
+var current = 0,
+    slides = document.getElementsByTagName("img");
+
+setInterval(function() {
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.opacity = 0;
+    }
+    current = (current != slides.length - 1) ? current + 1 : 0;
+    slides[current].style.opacity = 1;
+}, 3000);
